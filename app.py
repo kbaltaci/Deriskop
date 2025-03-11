92c7708d2187ee3ab6b96d34d9ca4718894f955c
kbaltaci <sercan.baltaci@gmail.com>
2025-03-11 12:44:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, request, jsonify, render_template
-import tensorflow as tf
-from tensorflow.keras.preprocessing import image
+from flask import Flask, request, render_template
 import numpy as np
+from tensorflow import lite
+from tensorflow.keras.preprocessing import image
 
 app = Flask(__name__)
 
-# Modeli yükle
-model = tf.keras.models.load_model('skin_cancer_model.h5')
+# TFLite modelini yükle
+interpreter = lite.Interpreter(model_path="skin_cancer_model.tflite")
+interpreter.allocate_tensors()
 
 # Görseli işlemek ve modelin tahmin yapabilmesi için uygun formata dönüştürmek
 def prepare_image(img):
@@ -15,30 +16,46 @@ def prepare_image(img):
     img_array = img_array / 255.0  # Ölçeklendirme
     return np.expand_dims(img_array, axis=0)  # Modelin tahmin yapabilmesi için boyutlandırma
 
+# TensorFlow Lite ile tahmin yapma
+def predict(image_array):
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    # Giriş verisini al
+    input_data = np.array(image_array, dtype=np.float32)
+
+    # Tahmin işlemi
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.invoke()
+
+    # Çıktıyı al
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    return output_data[0][0]  # Modelin çıktısını döndür
+
 @app.route('/')
 def home():
     return render_template('index.html')  # HTML formunu render et
 
 @app.route('/predict', methods=['POST'])
-def predict():
+def predict_image():
     if 'file' not in request.files:
-        return jsonify({"error": "No file part"})
+        return render_template('index.html', prediction="No file part")
     
     file = request.files['file']
     
     if file.filename == '':
-        return jsonify({"error": "No selected file"})
+        return render_template('index.html', prediction="No selected file")
     
     if file:
         img = image.load_img(file, target_size=(128, 128))
         img_array = prepare_image(img)
         
         # Modeli kullanarak tahmin yap
-        prediction = model.predict(img_array)
+        prediction = predict(img_array)
         
         # Sonuçları döndür
-        result = 'Malignant' if prediction[0] > 0.5 else 'Benign'
-        confidence = float(prediction[0]) * 100
+        result = 'Malignant' if prediction > 0.5 else 'Benign'
+        confidence = float(prediction) * 100
         
         return render_template('index.html', prediction=result, confidence=confidence)
 
