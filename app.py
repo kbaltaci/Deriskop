29f5d0f50bde50f68ae72f4d3054d3cd1fb54f74
kbaltaci <sercan.baltaci@gmail.com>
2025-03-11 15:39:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -55,4 +55,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Heroku'nun atadığı portu kullan
+    app.run(host="0.0.0.0", port=port, debug=True)
